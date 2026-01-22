f68a485c9d451f180ca474b92c1fb1f548cf4fa0
ruhtraBertoti <artgbertoti@gmail.com>
2026-01-22 01:09:06 +0000

    add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>Mariana, Eu te amo!!! E se tu me ama, me manda uma foto sorrido. XOXO!!</h1>;
+  return (
+    <h1>
+      Mariana, Eu te amo!!! E se tu me ama, me manda uma foto sorrido. XOXO!!
+    </h1>
+  );
 }
 
 export default Home;
